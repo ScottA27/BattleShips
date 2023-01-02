41f924e69cb78f2a2002801062b81adc9f4b0041
ScottA27 <scotta27501@gmail.com>
2023-01-02 19:24:08 +0000

    ADD: basic functions for building and printing the board

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from random import randint
 
-# global variables
-board_size = None
-
+board = []
 
 while True:
     board_size = input("Enter board size: ")
@@ -15,3 +13,26 @@ while True:
             print("You must enter a number between 3 and 10 in order to play!")
     else:
         print("You must enter a number between 3 and 10 in order to play!")
+        continue
+
+
+def build_board():
+    """
+    Gets the size of the board and builds it
+    """
+    for x in range(board_size):
+        board.append(["O"] * board_size)
+    return board_size
+
+
+def print_board():
+    """
+    Prints the board
+    """
+    for row in board:
+        print((" ").join(row))
+
+
+build_board()
+print_board()
+
