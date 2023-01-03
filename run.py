891e01b4444ff3df7ae1e3fa20a0c87ae69ddbf4
ScottA27 <scotta27501@gmail.com>
2023-01-03 19:22:06 +0000

    ADD: comprehensive function for building and placing ships

--- run.py
+++ run.py
@@ -1,55 +1,79 @@
 from random import randint
 
-board = []
+grid = []
+num_of_ships = 8
+all_ship_locations = []
+
 
 while True:
-    board_size = input("Enter board size: ")
-    if board_size.isdigit():
-        board_size = int(board_size)
-        if board_size >= 3 and board_size <= 10:
+    grid_size = input("Enter grid size: ")
+    if grid_size.isdigit():
+        grid_size = int(grid_size)
+        if grid_size >= 3 and grid_size <= 8:
             print("Well done now let's get this game started!")
             break
         else:
-            print("You must enter a number between 3 and 10 in order to play!")
+            print("You must enter a number between 3 and 8 in order to play!")
     else:
-        print("You must enter a number between 3 and 10 in order to play!")
+        print("You must enter a number between 3 and 8 in order to play!")
         continue
 
 
-def build_board():
+def build_grid():
     """
     Gets the size of the board and builds the rows
     """
-    for x in range(board_size):
-        board.append(["O"] * board_size)
-    return board_size
+    for x in range(grid_size):
+        grid.append(["O"] * grid_size)
+    return grid_size
 
 
 letters = "ABCDEFGHIJ"
 
 
-def print_board():
+def print_grid():
     """
-    Builds the entire board and prints it
+    Builds the entire grid and prints it
     """
-    column_letters = letters[0: (board_size)]
+    column_letters = letters[0: (grid_size)]
     print(" %s%s" % (" ", " ".join(column_letters)))
     row_number = 1
-    for row in board:
+    for row in grid:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
 
-build_board()
-print_board()
+def build_ships():
+    """
+    Building and placing the ships on the board
+    """
+    placed_ships = 0
+    if grid_size >= 3 and grid_size <= 4:
+        num_of_ships = 3
+        while placed_ships != num_of_ships:
+            ship_row = randint(1, (grid_size))
+            ship_column = randint(1, (grid_size))
+            ship_position = [ship_row, ship_column]
+            all_ship_locations.append(ship_position) 
+            placed_ships += 1
+    elif grid_size >= 5 and grid_size <= 6:
+        num_of_ships = 5
+        while placed_ships != num_of_ships:
+            ship_row = randint(1, (grid_size))
+            ship_column = randint(1, (grid_size))
+            ship_position = [ship_row, ship_column]
+            all_ship_locations.append(ship_position) 
+            placed_ships += 1
+    else:
+        num_of_ships = 10
+        while placed_ships != num_of_ships:
+            ship_row = randint(1, (grid_size))
+            ship_column = randint(1, (grid_size))
+            ship_position = [ship_row, ship_column]
+            all_ship_locations.append(ship_position) 
+            placed_ships += 1
+    
 
-def random_row():
-    return randint(1,len(board))
-
-
-def random_column():
-    return randint(1,len(board[0]))
-
-
-print(random_row())
-print(random_column())
+build_grid()
+print_grid()
+build_ships()
